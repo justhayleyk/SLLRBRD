5d775c586669c0ffcb4e30686cf3f779feaa1186
Hayley K <mrs.kornilenko@gmail.com>
2018-07-28 12:35:07 -0400

    created this intial JS file for the individualAds

---
+++ assets/javascript/individualPost.js
@@ -0,0 +1,23 @@
+'use strict';
+$(document).ready(function() {
+  var dbConfig = {
+    apiKey: 'AIzaSyDcOMILm8F_Mx8SD8B-SYkgGTxnTQ4nFXE',
+    authDomain: 'sb-sllrbrd.firebaseapp.com',
+    databaseURL: 'https://sb-sllrbrd.firebaseio.com',
+    projectId: 'sb-sllrbrd',
+    storageBucket: 'sb-sllrbrd.appspot.com',
+    messagingSenderId: '407280221848'
+  };
+  firebase.initializeApp(dbConfig);
+
+  var db = firebase.database();
+
+  // GLOBAL VARIABLE
+  var imageURL = '';
+  var description = '';
+  var title = '';
+  var price = 0;
+  var userID;
+
+  // Pull from database
+});
