8cd3da4e0bcedcd757a53b58b1edb1063d019b30
Hayley Sweet <mrs.kornilenko@gmail.com>
2018-07-28 17:20:31 -0400

    shortened the lenght of the description pulled into the html

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,12 +4,12 @@ $(document).ready(function() {
   // Initialize Firebase
   // var config = JSON.parse(localStorage.getItem('sbConfig'));
   var config = {
-    apiKey: 'AIzaSyDcOMILm8F_Mx8SD8B-SYkgGTxnTQ4nFXE',
-    authDomain: 'sb-sllrbrd.firebaseapp.com',
-    databaseURL: 'https://sb-sllrbrd.firebaseio.com',
-    projectId: 'sb-sllrbrd',
-    storageBucket: 'sb-sllrbrd.appspot.com',
-    messagingSenderId: '407280221848'
+    apiKey: "AIzaSyDcOMILm8F_Mx8SD8B-SYkgGTxnTQ4nFXE",
+    authDomain: "sb-sllrbrd.firebaseapp.com",
+    databaseURL: "https://sb-sllrbrd.firebaseio.com",
+    projectId: "sb-sllrbrd",
+    storageBucket: "sb-sllrbrd.appspot.com",
+    messagingSenderId: "407280221848"
   };
 
   firebase.initializeApp(config);
@@ -24,12 +24,12 @@ $(document).ready(function() {
   var posttitle;
   var postprice;
   var postuserID;
-  var html = '';
+  var html = "";
 
   var db = firebase.database();
-  var dbAds = db.ref('/ads'); // shorthand for data = db.re() &  data.child('ads')
+  var dbAds = db.ref("/ads"); // shorthand for data = db.re() &  data.child('ads')
 
-  dbAds.on('value', function(ads) {
+  dbAds.on("value", function(ads) {
     // FIRST GET THE SNAPSHOP FROM THE DB -- THIS IS AN OBJECT
     ads.forEach(function(ad) {
       // BEFORE WE CAN ACCESS THE PROPERTIES, WE NEED TO LOOP THROUGH THE OBJECT AND PASS IN THE ad
@@ -46,11 +46,12 @@ $(document).ready(function() {
         '"></div><div class="content" style="padding: 1rem;"><a class="header postTitle">' +
         posttitle +
         '</a><div class="postDescription"><p>' +
-        postdescription +
-        '</p></div><div class="postPrice"><h3>Price: <span class="price">' +
+        postdescription.slice(0, 250) +
+        " . . .   <br> <br><a href='#'> Read More</a>" +
+        '</p></div><br><div class="postPrice"><h3>Price: $<span class="price">' +
         postprice +
-        '</span></h3></div></div></div></div></div>';
-      $('.postItem').append(html);
+        "</span></h3></div></div></div></div></div>";
+      $(".postItem").append(html);
     });
   });
 
