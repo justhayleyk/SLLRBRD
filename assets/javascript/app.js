3c2e1a90d3d36adc78078c3a572919692ed078c9
Hayley K <mrs.kornilenko@gmail.com>
2018-07-28 15:30:36 -0400

    loading the ads on index

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,58 @@
 //MAIN JAVASCRIPT FILE -- We will pull the items over to this file.
-'use strict';
+// 'use strict';
 $(document).ready(function() {
-  var config = JSON.parse(localStorage.getItem('sbConfig'));
+  // Initialize Firebase
+  // var config = JSON.parse(localStorage.getItem('sbConfig'));
+  var config = {
+    apiKey: 'AIzaSyDcOMILm8F_Mx8SD8B-SYkgGTxnTQ4nFXE',
+    authDomain: 'sb-sllrbrd.firebaseapp.com',
+    databaseURL: 'https://sb-sllrbrd.firebaseio.com',
+    projectId: 'sb-sllrbrd',
+    storageBucket: 'sb-sllrbrd.appspot.com',
+    messagingSenderId: '407280221848'
+  };
+
   firebase.initializeApp(config);
 
-  var db = firebase.database();
-
+  // Storage Variable
   var store = firebase.storage();
+
+  // Database Variable
+
+  var postimageURL;
+  var postdescription;
+  var posttitle;
+  var postprice;
+  var postuserID;
+  var html = '';
+
+  var db = firebase.database();
+  var dbAds = db.ref('/ads'); // shorthand for data = db.re() &  data.child('ads')
+
+  dbAds.on('value', function(ads) {
+    // FIRST GET THE SNAPSHOP FROM THE DB -- THIS IS AN OBJECT
+    ads.forEach(function(ad) {
+      // BEFORE WE CAN ACCESS THE PROPERTIES, WE NEED TO LOOP THROUGH THE OBJECT AND PASS IN THE ad
+
+      postimageURL = ad.val().imageURL;
+      postdescription = ad.val().description;
+      posttitle = ad.val().title;
+      postprice = ad.val().price;
+      postuserID = ad.val().userID;
+
+      html =
+        '<div class="content"><div class="ui items"><div class="item"><div class="postItemImage ui small image"><img src="' +
+        postimageURL +
+        '"></div><div class="content" style="padding: 1rem;"><a class="header postTitle">' +
+        posttitle +
+        '</a><div class="postDescription"><p>' +
+        postdescription +
+        '</p></div><div class="postPrice"><h3>Price: <span class="price">' +
+        postprice +
+        '</span></h3></div></div></div></div></div>';
+      $('.postItem').append(html);
+    });
+  });
+
+  // bottom of on document ready
 });
