c70bb2cae022957a40f5bf0f9d1b930e7a0e9513
Hayley Sweet <mrs.kornilenko@gmail.com>
2018-07-28 21:47:12 -0400

    individual ad loads on click.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,6 @@
 //MAIN JAVASCRIPT FILE -- We will pull the items over to this file.
 // 'use strict';
+
 $(document).ready(function() {
   // Initialize Firebase
   // var config = JSON.parse(localStorage.getItem('sbConfig'));
@@ -33,7 +34,7 @@ $(document).ready(function() {
     // FIRST GET THE SNAPSHOP FROM THE DB -- THIS IS AN OBJECT
     ads.forEach(function(ad) {
       // BEFORE WE CAN ACCESS THE PROPERTIES, WE NEED TO LOOP THROUGH THE OBJECT AND PASS IN THE ad
-
+      postID = ad.key;
       postimageURL = ad.val().imageURL;
       postdescription = ad.val().description;
       posttitle = ad.val().title;
@@ -43,11 +44,13 @@ $(document).ready(function() {
       html =
         '<div class="content"><div class="ui items"><div class="item"><div class="postItemImage ui small image"><img src="' +
         postimageURL +
-        '"></div><div class="content" style="padding: 1rem;"><a class="header postTitle">' +
+        '"></div><div class="content" style="padding: 1rem;"><h2 class="header postTitle">' +
         posttitle +
-        '</a><div class="postDescription"><p>' +
+        '</h2><div class="postDescription"><p>' +
         postdescription.slice(0, 250) +
-        " . . .   <br> <br><a href='#'> Read More</a>" +
+        " . . .   <br> <br><a href='individualAd.html?postid=" +
+        postID +
+        "'> Read More</a>" +
         '</p></div><br><div class="postPrice"><h3>Price: $<span class="price">' +
         postprice +
         "</span></h3></div></div></div></div></div>";
