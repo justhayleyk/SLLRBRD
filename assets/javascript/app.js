3857f5f2b94d1efb587c3a6b23f8147fc7cabaf5
rmoj <rmoj99@gmail.com>
2018-07-31 19:06:30 -0400

    Capture field values.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,12 +5,12 @@ $(document).ready(function() {
   // Initialize Firebase
   // var config = JSON.parse(localStorage.getItem('sbConfig'));
   var config = {
-    apiKey: "AIzaSyDcOMILm8F_Mx8SD8B-SYkgGTxnTQ4nFXE",
-    authDomain: "sb-sllrbrd.firebaseapp.com",
-    databaseURL: "https://sb-sllrbrd.firebaseio.com",
-    projectId: "sb-sllrbrd",
-    storageBucket: "sb-sllrbrd.appspot.com",
-    messagingSenderId: "407280221848"
+    apiKey: 'AIzaSyDcOMILm8F_Mx8SD8B-SYkgGTxnTQ4nFXE',
+    authDomain: 'sb-sllrbrd.firebaseapp.com',
+    databaseURL: 'https://sb-sllrbrd.firebaseio.com',
+    projectId: 'sb-sllrbrd',
+    storageBucket: 'sb-sllrbrd.appspot.com',
+    messagingSenderId: '407280221848'
   };
 
   firebase.initializeApp(config);
@@ -25,12 +25,12 @@ $(document).ready(function() {
   var posttitle;
   var postprice;
   var postuserID;
-  var html = "";
+  var html = '';
 
   var db = firebase.database();
-  var dbAds = db.ref("/ads"); // shorthand for data = db.re() &  data.child('ads')
+  var dbAds = db.ref('/ads'); // shorthand for data = db.re() &  data.child('ads')
 
-  dbAds.on("value", function(ads) {
+  dbAds.on('value', function(ads) {
     // FIRST GET THE SNAPSHOP FROM THE DB -- THIS IS AN OBJECT
     ads.forEach(function(ad) {
       // BEFORE WE CAN ACCESS THE PROPERTIES, WE NEED TO LOOP THROUGH THE OBJECT AND PASS IN THE ad
@@ -53,10 +53,28 @@ $(document).ready(function() {
         "'> Read More</a>" +
         '</p></div><br><div class="postPrice"><h3>Price: $<span class="price">' +
         postprice +
-        "</span></h3></div></div></div></div></div>";
-      $(".postItem").append(html);
+        '</span></h3></div></div></div></div></div>';
+      $('.postItem').append(html);
     });
   });
+
+  function postAd(currentUser) {
+    var userID = currentUser;
+    var dbRef = db.ref('ads');
+    var title = $('#title')
+      .val()
+      .trim();
+    var description = $('#description')
+      .val()
+      .trim();
+    var price = $('#price')
+      .val()
+      .trim();
+    var file = $('#image').get(0).files[0];
+    var imgName = $('#image')
+      .val()
+      .trim();
+  }
 
   // bottom of on document ready
 });
