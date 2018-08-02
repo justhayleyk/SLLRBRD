233f4e1cfb044982eba77d424cf96769fd591d3a
rmoj <rmoj99@gmail.com>
2018-08-02 11:59:19 -0400

    Deleted dead code

--- assets/javascript/myAds.js
+++ assets/javascript/myAds.js
@@ -35,18 +35,7 @@ firebase.auth().onAuthStateChanged(function(user) {
 });
 
 $(document).ready(function() {
-  // Storage Variable
   var store = firebase.storage();
-
-  //User Session
-
-  // Database Variable
-
-  // var postimageURL;
-  // var postdescription;
-  // var posttitle;
-  // var postprice;
-  // var html = "";
 
   var db = firebase.database();
 
@@ -125,30 +114,6 @@ $(document).ready(function() {
         console.log('Error in deleting image:' + err);
       });
   }
-  // var dbPost = db.ref("/ads/" + urlParams.get("postid")); // shorthand for data = db.re() &  data.child('ads')
-
-  // dbPost.on("value", function(ad) {
-  //   postID = ad.key;
-  //   postimageURL = ad.val().imageURL;
-  //   postdescription = ad.val().description;
-  //   posttitle = ad.val().title;
-  //   postprice = ad.val().price;
-  //   postuserID = ad.val().userID;
-
-  //   html =
-  //     '<div class="row"><div class="six wide column"><img class="ui large rounded image" src="' +
-  //     postimageURL +
-  //     '" alt="Ad Image"></div><div class="eight wide column"><h2>' +
-  //     posttitle +
-  //     "</h2><p>" +
-  //     postdescription +
-  //     '</p><h3>Price:<span id="price">' +
-  //     postprice +
-  //     '</span></h3><br><div class="ui link items"><div class="item"><a class="ui tiny circular image"><img src="assets/images/userimage.png"></a><div class="content"><h3>' +
-  //     sessionuser.name +
-  //     '</h3><div class="description"><p>Contact:</p><div class="ui buttons"><button class="ui button">Call</button><div class="or"></div><button class="ui positive button">Email</button></div></div></div></div></div></div></div><div class="row"></div>';
-  //   $(".usersAd").append(html);
-  // });
 
   // bottom of on document ready
 });
